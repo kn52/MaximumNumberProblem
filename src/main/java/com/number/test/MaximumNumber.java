21f53f36cf2c7c62410950e804c8e2d33360187e
kn52 <ashish52922@gmail.com>
2020-02-28 16:58:42 +0530

    REFACTORED:Function to get maximum

--- src/main/java/com/number/test/MaximumNumber.java
+++ src/main/java/com/number/test/MaximumNumber.java
@@ -2,23 +2,7 @@ package com.number.test;
 
 public class MaximumNumber {
 
-    public int getMaximum(Integer x, Integer y, Integer z) {
-        if(x.compareTo(y)>0 && x.compareTo(z)>0)
-            return x;
-        if(y.compareTo(z)>0)
-            return y;
-        return z;
-    }
-
-    public float getMaximum(Float x, Float y, Float z) {
-        if(x.compareTo(y)>0 && x.compareTo(z)>0)
-            return x;
-        if(y.compareTo(z)>0)
-            return y;
-        return z;
-    }
-
-    public String getMaximum(String x, String y, String z) {
+    public static <E extends Comparable<E>> E getMaximum(E x, E y, E z) {
         if(x.compareTo(y)>0 && x.compareTo(z)>0)
             return x;
         if(y.compareTo(z)>0)
