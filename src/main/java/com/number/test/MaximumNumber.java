f30c2bba9eccfb45d09c4f93fe57bb06ec87a4d5
kn52 <ashish52922@gmail.com>
2020-02-28 16:20:51 +0530

    ADDED:Find greatest float number among three numbers

--- src/main/java/com/number/test/MaximumNumber.java
+++ src/main/java/com/number/test/MaximumNumber.java
@@ -2,7 +2,7 @@ package com.number.test;
 
 public class MaximumNumber {
 
-    public int getMaximumNumber(Integer x, Integer y, Integer z) {
+    public int getMaximum(Integer x, Integer y, Integer z) {
         Integer max=x;
         if(y.compareTo(max)>0)
             max=y;
@@ -11,5 +11,12 @@ public class MaximumNumber {
         return max;
     }
 
-
+    public float getMaximum(Float x, Float y, Float z) {
+        Float max=x;
+        if(y.compareTo(max)>0)
+            max=y;
+        if(z.compareTo(max)>0)
+            max=z;
+        return max;
+    }
 }
--- src/test/java/com/number/test/MaximumNumberTest.java
+++ src/test/java/com/number/test/MaximumNumberTest.java
@@ -7,26 +7,44 @@ import org.junit.Test;
 public class MaximumNumberTest {
     MaximumNumber maxNumber;
     @Before
-    public void setUp() {
+    public void setUp() throws Exception{
         maxNumber=new MaximumNumber();
     }
 
     @Test
     public void givenFirstIntegerNumber_IsGreater_ShouldReturn_FirstNumber() {
-        int maximum=maxNumber.getMaximumNumber(8,4,5);
+        int maximum=maxNumber.getMaximum(8,4,5);
         Assert.assertEquals(8,maximum);
     }
 
     @Test
     public void givenSecondIntegerNumber_IsGreater_ShouldReturn_SecondNumber() {
-        int maximum=maxNumber.getMaximumNumber(6,9,3);
+        int maximum=maxNumber.getMaximum(6,9,3);
         Assert.assertEquals(9,maximum);
     }
 
     @Test
     public void givenThirdIntegerNumber_IsGreater_ShouldReturn_ThirdNumber() {
-        int maximum=maxNumber.getMaximumNumber(3,4,7);
+        int maximum=maxNumber.getMaximum(3,4,7);
         Assert.assertEquals(7,maximum);
+    }
+
+    @Test
+    public void givenFirstFloatNumber_IsGreater_ShouldReturn_FirstNumber() {
+        float maximum=maxNumber.getMaximum(8.7f,4.6f,5.5f);
+        Assert.assertEquals(8.7f,maximum,0.0);
+    }
+
+    @Test
+    public void givenSecondFloatNumber_IsGreater_ShouldReturn_SecondNumber() {
+        float maximum=maxNumber.getMaximum(6.5f,9.9f,3.1f);
+        Assert.assertEquals(9.9f,maximum,0.0);
+    }
+
+    @Test
+    public void givenThirdFloatNumber_IsGreater_ShouldReturn_ThirdNumber() {
+        float maximum=maxNumber.getMaximum(3.2f,4.6f,7.2f);
+        Assert.assertEquals(7.2f,maximum,0.0);
     }
 }
 
